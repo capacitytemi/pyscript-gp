fa10fdc22c41b582351b55a77e210e3a58f69939
Emmanuel <capacitytemi@gmail.com>
2022-10-03 01:30:12 +0100

    update 1.1.1x

--- starter_files/src/helper.py
+++ starter_files/src/helper.py
@@ -10,3 +10,17 @@ from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 
 
+def preprocess(data):
+    data = data.drop(['model', 'transmission', 'fuelType'], axis = 1)
+    return data
+
+def split(data):
+    X = data.drop(['price'], axis=1)
+    y = data['price']
+    X_train,X_test,y_train,y_test = train_test_split(X,y, test_size=0.2, random_state=25)
+    return X_train, X_test, y_train, y_test
+
+def lr(X,y):
+    regressor = LinearRegression()
+    regressor.fit(X, y)
+    return regressor
